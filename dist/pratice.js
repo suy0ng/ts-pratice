3726f8eabb4e72f0d7eab61fc658d922d31d2783
suy0ng <yunsuyong739@naver.com>
2023-04-27 08:25:49 +0900

    update dist

--- dist/pratice.js
+++ dist/pratice.js
@@ -1,3 +1,22 @@
 "use strict";
-const message = 'hello world!';
-console.log(message);
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+    getArea() {
+        return this.radius * this.radius * Math.PI;
+    }
+}
+class Rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    getArea() {
+        return this.width * this.height;
+    }
+}
+const shapes = [new Circle(5), new Rectangle(10, 5)];
+shapes.forEach((shape) => {
+    console.log(shape.getArea());
+});
