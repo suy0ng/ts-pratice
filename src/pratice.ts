e5d21b979826284b29667f306fc9c6cd3591bc2b
suy0ng <yunsuyong739@naver.com>
2023-04-27 08:24:12 +0900

    interface 사용해보기

--- src/pratice.ts
+++ src/pratice.ts
@@ -1,15 +1,37 @@
-function sum(x: number, y: number): number {
-  return x + y;
+interface Shape {
+  getArea(): number; // Shape interface 에는 getArea 라는 함수가 꼭 있어야 하며 해당 함수의 반환값은 숫자입니다.
 }
 
-sum(1, 2);
+class Circle implements Shape {
+  // 'implements' 키워드를 사용하여 해당 클래스가 Shape interface 의 조건을 충족하겠다는 것을 명시합니다.
 
-function sumArray(numbers: number[]): number {
-  return numbers.reduce((acc, current) => acc + current, 0);
+  radius: number;
+
+  constructor(radius: number) {
+    this.radius = radius;
+  }
+
+  getArea() {
+    return this.radius * this.radius * Math.PI;
+  }
 }
 
-const total = sumArray([1, 2, 3, 4, 5]);
+class Rectangle implements Shape {
+  width: number;
+  height: number;
 
-function returnNothing(): void{
-    console.log("I am just saying hello world!")
+  constructor(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+  }
+
+  getArea(): number {
+    return this.width * this.height;
+  }
 }
+
+const shapes: Shape[] = [new Circle(5), new Rectangle(10, 5)];
+
+shapes.forEach((shape) => {
+  console.log(shape.getArea());
+});
