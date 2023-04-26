edc359da55c793f8b8d6780a68f60f974425af03
suy0ng <yunsuyong739@naver.com>
2023-04-26 18:07:51 +0900

    tsc 적용

--- src/pratice.ts
+++ src/pratice.ts
@@ -9,11 +9,11 @@ const done: boolean = true; //불리언 값
 const numbers: number[] = [1, 2, 3]; //숫자 배열
 const messages: string[] = ["Hello", "World!", "TypeScripts"]; // 문자열 배열
 
-// messages.push(1) // 숫자를 넣으려고 하면 오류 발생!
+// messages.push(1)                               // 숫자를 넣으려고 하면 오류 발생!
 
 let mightBeUndefined: string | undefined = undefined; // string일수도 있고 undifined일수도 있다.
-let nullableNumber: number | null = null; // number일수도 있고 null 일수도 있음
+let nullableNumber: number | null = null;        // number일수도 있고 null 일수도 있음
 
 let color: "red" | "orange" | "yellow" = "red"; // red,orange,yellow 중 하나임
 color = "yellow";
-// color = 'green'                        // color에 Green을 넣을수 없음! 에러가 발생한다
+// color = 'green'                             // color에 Green을 넣을수 없음! 에러가 발생한다
