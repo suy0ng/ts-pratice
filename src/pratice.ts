606e51ee0de8efcc715941171d05a8b35c6072c6
suy0ng <yunsuyong739@naver.com>
2023-04-26 18:13:52 +0900

    함수에서 타입 정의하기

--- src/pratice.ts
+++ src/pratice.ts
@@ -1,19 +1,15 @@
-let count = 0; // 숫자
-count += 1;
-// count = '문자열' //이러면 에러가 발생한다
+function sum(x: number, y: number): number {
+  return x + y;
+}
 
-const message: string = "Hello World!!"; // 문자열
+sum(1, 2);
 
-const done: boolean = true; //불리언 값
+function sumArray(numbers: number[]): number {
+  return numbers.reduce((acc, current) => acc + current, 0);
+}
 
-const numbers: number[] = [1, 2, 3]; //숫자 배열
-const messages: string[] = ["Hello", "World!", "TypeScripts"]; // 문자열 배열
+const total = sumArray([1, 2, 3, 4, 5]);
 
-// messages.push(1)                               // 숫자를 넣으려고 하면 오류 발생!
-
-let mightBeUndefined: string | undefined = undefined; // string일수도 있고 undifined일수도 있다.
-let nullableNumber: number | null = null;        // number일수도 있고 null 일수도 있음
-
-let color: "red" | "orange" | "yellow" = "red"; // red,orange,yellow 중 하나임
-color = "yellow";
-// color = 'green'                             // color에 Green을 넣을수 없음! 에러가 발생한다
+function returnNothing(): void{
+    console.log("I am just saying hello world!")
+}
