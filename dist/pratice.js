9de6f06ac5b37f1c937e23d3da6b71433432422f
suy0ng <yunsuyong739@naver.com>
2023-04-27 10:18:18 +0900

    Generics interface , class 에서 사용하기

--- dist/pratice.js
+++ dist/pratice.js
@@ -1,10 +1,30 @@
 "use strict";
-const person = {
-    name: '김사람',
-    age: 20
+const items = {
+    list: ['a', 'b', 'c', 'd']
 };
-const expert = {
-    name: "r김 개발",
-    skills: ['react', 'typescripts', 'javascripts']
-};
-const poeple = [person, expert];
+// Class에서 Generic 사용하기
+class Queue {
+    constructor() {
+        this.list = [];
+    }
+    get length() {
+        return this.list.length;
+    }
+    enqueue(item) {
+        this.list.push(item);
+    }
+    dequeue() {
+        return this.list.shift();
+    }
+}
+const queue = new Queue();
+queue.enqueue(0);
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
--- src/pratice.ts
+++ src/pratice.ts
@@ -1,39 +1,34 @@
-// 예제 1
-// function merge(a: any, b: any): any {
-//   return {
-//     ...a,
-//     ...b,
-//   };
-// }
+// Interface에서 Generic 사용하기
+interface Items<T>{
+  list:T[];
+}
 
-// const merged = merge({ foo: 1 }, { bar: 1 });
+const items: Items<string> = {
+  list: ['a','b','c','d']
+}
 
-// Generic 사용
-// function merge(a: any, b: any): any {
-//   return {
-//     ...a,
-//     ...b
-//   };
-// }
-
-// const merged = merge({ foo: 1 }, { bar: 1 });
-
-//Generic 사용 2
-// function merge<A, B>(a: A, b: B): A & B {
-//   return {
-//     ...a,
-//     ...b,
-//   };
-// }
-
-// const merged = merge({ foo: 1 }, { bar: 1 });
-
-
-// Generic 사용 3
-function wrap<T>(param: T){
-  return{
-    param
+// Class에서 Generic 사용하기
+class Queue<T> {
+  list: T[] = [];
+  get length() {
+    return this.list.length;
+  }
+  enqueue(item: T){
+    this.list.push(item);
+  }
+  dequeue(){
+    return this.list.shift();
   }
 }
 
-const wrapped = wrap(10);
+const queue = new Queue<number>();
+queue.enqueue(0);
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
